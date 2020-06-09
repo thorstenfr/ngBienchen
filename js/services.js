bf8cb9e2ee8d0ffb60c0346dfe5c32c852d8faf6
Thorsten Freimann <thorstenfr@users.noreply.github.com>
2020-06-09 10:54:26 +0200

    Kurs- und Teilnehmer Schnelleingabe: Anzeige auch, wenn kein Kurs oder Teilnehmer.

--- js/services.js
+++ js/services.js
@@ -33,7 +33,7 @@ angular.module('app.services', [])
         },
         isRealdrive: function() {
             // false, falls im Browser
-            return true;
+            return false;
         },
 		newConfig: function() {
 			return {
